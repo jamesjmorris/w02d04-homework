721bbd22959dc04c3df75b0aecc3c1344bb643f7
James M <saintjames@MacBook-Pro-2.local>
2018-08-01 21:40:38 -0600

    Commit 1 - I added my first element and it was fun

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -3,3 +3,11 @@
 //   } else {
 //     console.log('I did it! I linked jQuery and this js file!')
 //   };
+
+const $container = document.getElementById("container");
+
+const $title = document.getElementById("title");
+
+console.log($container);
+
+console.log($title);
