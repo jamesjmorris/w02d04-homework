0d8db7b34f6a5c362337bc0de4fcf4afc58e0512
James M <saintjames@MacBook-Pro-2.local>
2018-08-15 22:41:19 -0600

    Commit 5 - The days go by slowly and the years go by too fast

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -4,10 +4,29 @@
 //     console.log('I did it! I linked jQuery and this js file!')
 //   };
 
+const $wand = document.getElementById("wand");
+
+$("#wand").css("color", "indigo");
+
 const $container = document.getElementById("container");
+const $bag = document.getElementById("bag");
 
 const $title = document.getElementById("title");
 
 console.log($container);
 
 console.log($title);
+
+// Removing Wand
+document.getElementById("wand").remove();
+
+// Removing butter beer
+document.getElementById("bb").remove();
+
+// Addind Wand
+$($container).append("<h4 class='wand'>Hawthorn Wand with Dragon Heartstring Core</h4>");
+$(".wand").css({"color": "indigo"});
+
+const rat = document.getElementsByClassName("rat");
+$(rat).appendTo($container)
+$(rat).appendTo($bag)
