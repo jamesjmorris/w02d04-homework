6df99d21dbfc08b3ddf0662d4221def991bb5e01
James M <saintjames@MacBook-Pro-2.local>
2018-08-16 21:16:00 -0600

    Commit 7 - I am getting more comfortable with ambiguous instructions

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -1,32 +1,63 @@
-// if (typeof jQuery == 'undefined'){
-//     console.log('oops! I still have to link my jQuery properly!');
-//   } else {
-//     console.log('I did it! I linked jQuery and this js file!')
-//   };
-
-const $wand = document.getElementById("wand");
-
-$("#wand").css("color", "indigo");
-
-const $container = document.getElementById("container");
-const $bag = document.getElementById("bag");
-
-const $title = document.getElementById("title");
-
+// Year 1
+const $container = $('#container');
 console.log($container);
+const $h1 = $('<h1></h1>');
+$h1.text('Hogwarts');
+$container.append($h1);
 
-console.log($title);
+// Year 2
+const $name = $('<h2>James</h2>');
+$container.append($name);
+const $house = $('<h3>Slytherin</h3>');
+$container.append($house);
+const $rat = $('<h4 class="rat">Rattata</h4>');
+$container.append($rat);
+const $wand = $('<h4>Birch Wand with Phoenix Feather Core</h4>');
+$container.append($wand);
 
-// Removing Wand
-document.getElementById("wand").remove();
+// Year 3
+const $ul = $('<ul storage="trunk"></ul>');
 
-// Removing butter beer
-document.getElementById("bb").remove();
+const $bb = $('<li>butter beer</li>')
+$ul.append($bb);
+const $ic = $('<li class="secret">invisibility cloak</li>');
+$ul.append($ic);
+const $mm = $('<li class="secret">magic map</li>');
+$ul.append($mm);
+const $tt = $('<li class="secret">time turner</li>');
+$ul.append($tt);
+const $leash = $('<li class="rat">leash</li>');
+$ul.append($leash);
+const $beans = $("<li>Bertie Bott's Every Flavor [Jelly] Beans</li>");
+$ul.append($beans);
 
-// Addind Wand
-$($container).append("<h4 class='wand'>Hawthorn Wand with Dragon Heartstring Core</h4>");
-$(".wand").css({"color": "indigo"});
+$container.append($ul);
 
-const rat = document.getElementsByClassName("rat");
-$(rat).appendTo($container)
-$(rat).appendTo($bag)
+// Year 4
+const $title = $('<h5>Spring 2017</h5>');
+$container.append($title)
+const $table = $('<table><thead><tr><th>Day</th><th>Classes</th></tr><tr><td>Monday</td><td>History of Magic</td></tr><tr><td>Tuesday</td><td>Potions</td></tr><tr><td>Wednesday</td><td>Quidditch Practice</td></tr><tr><td>Thursday</td><td>Transfiguration</td></tr><tr><td>Friday</td><td>Defense Against the Dark Arts</td></tr><tr><td>Saturday</td><td>Herbology</td></tr><tr><td>Sunday</td><td>Divination</td></tr></thead></table>');
+$container.append($table);
+
+// Year 5
+$wand.remove();
+$bb.remove();
+$wand.insertAfter($rat);
+
+$wand.css('color', 'indigo');
+
+$rat.insertAfter($container);
+$rat.insertAfter($house);
+
+// Year 6
+$('.secret').hide().delay(2000).show('slow');
+$leash.addClass('cabbage');
+$leash.removeClass('cabbage');
+
+// Year 7
+$title.text('Fall 2018');
+$ul.prepend('<li>butter beer</li>');
+$ul.removeAttr('storage');
+$ul.attr('storage', 'chest');
+const $potter = $('<img src="https://s3.amazonaws.com/freebiesupply/thumbs/2x/harry-potter-logo.png">');
+$potter.insertBefore($h1);
